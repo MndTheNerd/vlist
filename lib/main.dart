975baac3b40af485ca02d86b0b6af6ff69e5eb37
MndTheNerd <mndthenerd@gmail.com>
2021-01-06 20:57:24 +0300

    working on firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 // Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
 
 void main() {
-  runApp(App());
+  runApp(MyApp());
 }
 
-class App extends StatelessWidget {
+class MyApp extends StatelessWidget {
   // Create the initialization Future outside of `build`:
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
@@ -19,17 +21,40 @@ class App extends StatelessWidget {
       builder: (context, snapshot) {
         // Check for errors
         if (snapshot.hasError) {
-          return SomethingWentWrong();
+          return Scaffold(
+            body: Center(
+              child: Text('Error'),
+            ),
+          );
         }
 
         // Once complete, show your application
         if (snapshot.connectionState == ConnectionState.done) {
-          return MyAwesomeApp();
+          return Root();
         }
 
         // Otherwise, show something whilst waiting for initialization to complete
-        return Loading();
+        return Scaffold(
+          body: Center(
+            child: Text('loading'),
+          ),
+        );
       },
     );
+  }
+}
+
+class Root extends StatefulWidget {
+  @override
+  _RootState createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(stream: , //Auth);
   }
 }
